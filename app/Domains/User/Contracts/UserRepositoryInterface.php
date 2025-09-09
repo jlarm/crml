f8454f96adc711aefb6c7ebddd120d206c9e2d68
jlarm <117611704+jlarm@users.noreply.github.com>
2025-09-09 10:10:43 -0500

    Added user contract

---
+++ app/Domains/User/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\User\Contracts;
+
+use App\Domains\User\DTOs\CreateUserDTO;
+use App\Domains\User\DTOs\UpdateUserDTO;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+interface UserRepositoryInterface
+{
+    public function all(): Collection;
+
+    public function find(int $id): ?User;
+
+    public function findOrFail(int $id): User;
+
+    public function create(CreateUserDTO $dto): User;
+
+    public function update(UpdateUserDTO $dto): User;
+
+    public function delete(int $id): bool;
+}
